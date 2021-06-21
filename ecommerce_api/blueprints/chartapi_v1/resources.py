4772f5c0b55f02f93fb2dada70eb0dbb5d86a158
Victor Martinez <victormartinez@bankai-2.local>
2021-06-21 10:32:33 -0300

    feat: check product ids in chart endpoint

--- ecommerce_api/blueprints/chartapi_v1/resources.py
+++ ecommerce_api/blueprints/chartapi_v1/resources.py
@@ -1,10 +1,10 @@
 from flask import request
 from flask_restful import Resource
 
-from ecommerce_api.ext.database import db
 from ecommerce_api.constants import ResponseCode
 from ecommerce_api.blueprints.presenter import create_response, exc_to_str
 from ecommerce_api.blueprints.chartapi_v1.schema import parse_payload
+from ecommerce_api.repositories import ProductRepository
 
 
 class ChartResource(Resource):
@@ -18,5 +18,15 @@ class ChartResource(Resource):
                 message=exc_to_str(data_or_exc),
             )
 
+        requested_ids = [d["id"] for d in data_or_exc["products"]]
+        invalid_ids = ProductRepository.get_invalid_ids(requested_ids)
+        if invalid_ids:
+            return create_response(
+                400,
+                code=ResponseCode.PRODUCTS_NOT_FOUND.value,
+                message="One or more products are invalid.",
+                data={"ids": invalid_ids}
+            )
+
         # TODO: implement logic
-        return create_response(200, data=db)
+        return create_response(200)
--- ecommerce_api/constants/response.py
+++ ecommerce_api/constants/response.py
@@ -5,6 +5,4 @@ class ResponseCode(enum.Enum):
 
     NOT_AUTHORIZED = "not_authorized"
     INVALID_PAYLOAD = "invalid_payload"
-    OBJECT_NOT_FOUND = "object_not_found"
-    OBJECT_NOT_UPDATED = "object_not_updated"
-    OBJECT_NOT_CREATED = "object_not_created"
+    PRODUCTS_NOT_FOUND = "products_not_found"
