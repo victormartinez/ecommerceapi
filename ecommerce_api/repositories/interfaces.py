fe0288cc5696587f0800e4b1a309e813d538485d
Victor Martinez <victormartinez@bankai-2.local>
2021-06-22 11:55:57 -0300

    feat: create interface for Repository

---
+++ ecommerce_api/repositories/interfaces.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+from typing import List, Dict
+
+
+class AbstractRepository(ABC):
+
+    @abstractmethod
+    def all(self) -> List[Dict]:
+        pass
+
+    @abstractmethod
+    def filter_by(self, kv: Dict):
+        pass
+
+    @abstractmethod
+    def find_by_ids(self, ids: List[int]) -> List[Dict]:
+        pass
