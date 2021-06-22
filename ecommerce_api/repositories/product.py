647172a0be1758c527f6a7daedcd93a8782c5c0f
Victor Martinez <victormartinez@bankai-2.local>
2021-06-22 10:38:39 -0300

    refactor: rename repository method

--- ecommerce_api/repositories/product.py
+++ ecommerce_api/repositories/product.py
@@ -6,7 +6,7 @@ class ProductRepository:
     def __init__(self, db: List[Dict]):
         self.db = db
 
-    def filter_by_id(self, ids: List[int]) -> List[Dict]:
+    def filter_by_ids(self, ids: List[int]) -> List[Dict]:
         return list(filter(lambda x: x["id"] in ids, self.db))
 
     def get_invalid_ids(self, ids: List[int]) -> List[int]:
