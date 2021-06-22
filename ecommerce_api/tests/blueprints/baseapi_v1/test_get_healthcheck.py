46d3fc6b8f39f543efe749b9dad0aea2d18fad44
Victor Martinez <victormartinez@bankai-2.local>
2021-06-22 15:49:58 -0300

    test: fix healthcheck testcase

--- ecommerce_api/tests/blueprints/baseapi_v1/test_get_healthcheck.py
+++ ecommerce_api/tests/blueprints/baseapi_v1/test_get_healthcheck.py
@@ -1,4 +1,4 @@
-def test__healthcheck__success(app, database):
+def test__healthcheck__success(app):
     with app.test_client() as client:
         response = client.get("/")
         assert response == 200
