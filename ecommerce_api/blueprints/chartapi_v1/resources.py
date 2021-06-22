cc7bdd79cc4cafd7fd591db85836785f31b91f23
Victor Martinez <victormartinez@bankai-2.local>
2021-06-22 10:41:03 -0300

    feat: return data processed by pipeline

--- ecommerce_api/blueprints/chartapi_v1/resources.py
+++ ecommerce_api/blueprints/chartapi_v1/resources.py
@@ -1,7 +1,15 @@
 from flask import request
 from flask_restful import Resource
 
+import settings
 from ecommerce_api.constants import ResponseCode
+from ecommerce_api.core.discount import DiscountClient
+from ecommerce_api.core.chart import (
+    ChartPipeline,
+    Context,
+    dict_to_products,
+    exceptions,
+)
 from ecommerce_api.ext.database import db
 from ecommerce_api.repositories import ProductRepository
 from ecommerce_api.blueprints.presenter import create_response, exc_to_str
@@ -19,16 +27,22 @@ class ChartResource(Resource):
                 message=exc_to_str(data_or_exc),
             )
 
-        product_repository = ProductRepository(db)
-        requested_ids = [d["id"] for d in data_or_exc["products"]]
-        invalid_ids = product_repository.get_invalid_ids(requested_ids)
-        if invalid_ids:
+        return self._process_chart(data_or_exc["products"])
+
+    def _process_chart(self, products_data):
+        try:
+            discount_client = DiscountClient(settings.DISCOUNT_SERVICE_HOST, settings.DISCOUNT_SERVICE_PORT)
+            chart_products = dict_to_products(products_data, ProductRepository(db))
+            data = ChartPipeline(Context(
+                chart_products=chart_products,
+                discount_client=discount_client,
+                black_friday_date=settings.BLACK_FRIDAY_DATE,
+            )).process()
+            return create_response(200, data=data)
+        except exceptions.ProductsNotFound as exc:
             return create_response(
                 400,
                 code=ResponseCode.PRODUCTS_NOT_FOUND.value,
-                message="One or more products are invalid.",
-                data={"ids": invalid_ids}
+                message=exc.message,
+                data={"ids": exc.product_ids}
             )
-
-        # TODO: implement logic
-        return create_response(200)
